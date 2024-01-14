d62d4aa4b34c31e2cfe8ae30d76bfc34e1334743
LiuZiqing277 <2153853451@qq.com>
2024-01-14 18:46:16 +0800

    Site updated: 2024-01-14 18:46:16

---
+++ js/baidu_tongji.js
@@ -0,0 +1,10 @@
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+  var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?91d17d7ecd40c7ddd276247333cbf42f";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
